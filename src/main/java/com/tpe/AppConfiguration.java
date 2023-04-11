fa3332932669369ee5df1c09965464119bb44c2a
İlker Şelimen <ilkerselimen@gmail.com>
2023-04-11 23:25:15 +0300

    v02

--- src/main/java/com/tpe/AppConfiguration.java
+++ src/main/java/com/tpe/AppConfiguration.java
@@ -1,12 +1,19 @@
 package com.tpe;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Random;
 
 @Configuration // bu classda config olacak
 @ComponentScan("com.tpe")// bu packagedaki componentlari tara
 public class AppConfiguration {
 
+    @Bean // random classindan bir tane bean olusturuluyor.
+    public Random random(){
+        return new Random();
+    }
 
 
 }
